3bdfb50b3248aeb87bbbd185ab460258fa9e977f
Caumannerman <uigohiiming2@gmail.com>
2021-11-13 00:16:45 +0900

    feat: link question create API

--- DesireApp/pages/Questions.js
+++ DesireApp/pages/Questions.js
@@ -1,27 +1,38 @@
 import React, {useState} from 'react';
 import styled from 'styled-components/native';
 import LinearGradient from 'react-native-linear-gradient';
-import {TextInput, Text, Image, TouchableOpacity, View, ScrollView, Modal, Dimensions} from 'react-native';
+import {
+  TextInput,
+  Text,
+  Image,
+  TouchableOpacity,
+  View,
+  ScrollView,
+  Modal,
+  Dimensions,
+} from 'react-native';
 import SimpleModal from '../components/SimpleModal';
 import produce from 'immer';
+import QuestionService from '../services/QuestionService';
+import UploadedAudioService from '../services/UploadedAudioService';
+import UploadedImageService from '../services/UploadedImageService';
+import UploadedFileService from '../services/UploadedFileService';
+import UploadedVideoService from '../services/UploadedVideoService';
 
 const Container = styled.SafeAreaView`
   flex: 1;
 `;
 const Background = styled(LinearGradient)`
-  flex: 1;  
-
+  flex: 1;
 `;
 const Contents = styled.View`
   flex: 1;
-  
+
   flex-direction: column;
   align-items: center;
-  
 `;
 
 const TitleView = styled.View`
-
   margin-top: 7%;
   margin-left: 10%;
 
@@ -36,10 +47,7 @@ const Title = styled.Text`
   font-weight: 800;
   color: #000000;
   font-style: normal;
-  
 `;
-
-
 
 const File = styled.View`
   width: 100%;
@@ -53,10 +61,9 @@ const File = styled.View`
 const FileOpacity = styled.TouchableOpacity`
   width: 25%;
   height: 100%;
-  
+
   justify-content: center;
   align-items: center;
-
 `;
 
 const FileText = styled.Text`
@@ -66,112 +73,214 @@ const FileText = styled.Text`
   font-style: normal;
 `;
 
-
-function Question(props){
+function Question(props) {
   const [isModalVisible, setisModalVisible] = useState(false);
   const [chooseData, setchooseData] = useState();
+  const [questionTitle, setQuestionTitle] = useState('');
+  const [questionText, setQuestionText] = useState('');
 
-  const changeModalVisible = (bool) => {
+  const changeModalVisible = bool => {
     setisModalVisible(bool);
-  }
+  };
 
-  const setData = (data) => {
+  const setData = data => {
     setchooseData(data);
-  }
+  };
 
-  const [list, setList] = useState([
-    {category: 'UIUX', isCheck: false},
-    {category: 'BIBX', isCheck: false},
-    {category: '제품디자인', isCheck: false},
-    {category: '시각디자인', isCheck: false},
+  const [categories, setCategories] = useState([
+    {category: 'UIUX', value: 'UI/UX', isCheck: false},
+    {category: 'BIBX', value: 'BI/BX', isCheck: false},
+    {category: '제품디자인', value: '제품디자인', isCheck: false},
+    {category: '시각디자인', value: '시각디자인', isCheck: false},
   ]);
 
+  const TEMP_USER_ID = '8136385e-42af-493f-a938-f7b6fdc97e69';
 
-    return(
-    
-      <Container>
-        <Background colors={['#ffffff','#f8ecec','#ffffff']} start={{x: 0.3, y: 0.3}} end={{x: 1.2, y: 1.2}} locations={[0,0.3,0.7]} >
-            <Contents>
+  const onSubmit = async () => {
+    const selectedCategoryNames = categories
+      .filter(category => category.isCheck)
+      .map(selectedCategory => selectedCategory.value);
 
+    await QuestionService.create({
+      userId: TEMP_USER_ID,
+      title: questionTitle,
+      questionText,
+      categories: selectedCategoryNames,
+    }).then(() => {
+      changeModalVisible(true);
+    });
+  };
 
-              <TitleView>
-                <Title>질문하기</Title>
-              </TitleView>
+  return (
+    <Container>
+      <Background
+        colors={['#ffffff', '#f8ecec', '#ffffff']}
+        start={{x: 0.3, y: 0.3}}
+        end={{x: 1.2, y: 1.2}}
+        locations={[0, 0.3, 0.7]}>
+        <Contents>
+          <TitleView>
+            <Title>질문하기</Title>
+          </TitleView>
 
-              <View style={{width:'100%', height:40, flexDirection:'row'}}>
-                <View style={{width:'30%', alignItems:'center', justifyContent:'center', backgroundColor:'#e5e5e5', borderWidth:0.4, borderColor:'#838383'}}><Text>제목</Text></View>
-                <TextInput placeholder="    제목 입력"  style={{borderWidth:3, borderColor:'#ff0000', width:"70%",borderWidth:0.4, borderColor:'#838383'}}></TextInput>
-              </View>
+          <View style={{width: '100%', height: 40, flexDirection: 'row'}}>
+            <View
+              style={{
+                width: '30%',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: '#e5e5e5',
+                borderWidth: 0.4,
+                borderColor: '#838383',
+              }}>
+              <Text>제목</Text>
+            </View>
+            <TextInput
+              placeholder="    제목 입력"
+              style={{
+                borderWidth: 3,
+                borderColor: '#ff0000',
+                width: '70%',
+                borderWidth: 0.4,
+                borderColor: '#838383',
+              }}
+              onChangeText={text => setQuestionTitle(text)}></TextInput>
+          </View>
 
+          <View
+            style={{
+              width: '100%',
+              height: 65,
+              alignItems: 'center',
+              flexDirection: 'row',
+              backgroundColor: '#e5e5e5',
+              borderWidth: 0.2,
+              borderColor: '#838383',
+            }}>
+            <View
+              style={{
+                width: '30%',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <Text>카테고리 선택</Text>
+            </View>
+            <ScrollView style={{height: '100%'}} horizontal={true}>
+              {categories.map(item => (
+                <TouchableOpacity
+                  onPress={() => {
+                    setCategories(
+                      produce(categories, draft => {
+                        const index = categories.indexOf(item);
+                        draft[index].isCheck = !categories[index].isCheck;
+                      }),
+                    );
+                  }}
+                  style={{
+                    height: 41,
+                    marginTop: 12,
+                    marginLeft: 15,
+                    borderRadius: 20,
+                    backgroundColor: item.isCheck ? '#952bff' : '#e5e5e5',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}>
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      color: item.isCheck ? '#ffffff' : '#000000',
+                      fontWeight: item.isCheck ? 'bold' : '300',
+                      marginHorizontal: 10,
+                    }}>
+                    {item.category}
+                  </Text>
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+          </View>
 
-              <View style={{width:'100%', height:65,alignItems:'center', flexDirection:'row', backgroundColor:'#e5e5e5', borderWidth:0.2, borderColor:'#838383'}}>
-                <View style={{width:'30%', alignItems:'center', justifyContent:'center'}}><Text>카테고리 선택</Text></View>
-                <ScrollView  style={{ height:'100%'}}  horizontal={true}>
-                  
-                  {list.map( item =>
-                              <TouchableOpacity onPress={()=>{ setList( produce( list, draft =>{
-                                  const index = list.indexOf(item);
-                                  draft[index].isCheck = !list[index].isCheck;
-
-                              }))}} style={{ height:41,marginTop:12, marginLeft:15, borderRadius:20, backgroundColor:(item.isCheck?'#952bff':'#e5e5e5'), alignItems:'center', justifyContent:'center'}}>
-                                <Text style={{fontSize:14, color:(item.isCheck?'#ffffff':'#000000'), fontWeight:(item.isCheck?'bold':'300'), marginHorizontal:10}}>{item.category}</Text>
-                              </TouchableOpacity>
-                          )}
-                </ScrollView>
-              </View>
-
-              <View style={{borderWidth:0.4, borderColor:'#a0a0a0', width:'100%', height:'35%', alignItems:'center'}}>
-                <TextInput style={{ width:'90%',height:'100%'}} placeholder={`궁금한 내용을 질문해주세요 
+          <View
+            style={{
+              borderWidth: 0.4,
+              borderColor: '#a0a0a0',
+              width: '100%',
+              height: '35%',
+              alignItems: 'center',
+            }}>
+            <TextInput
+              style={{width: '90%', height: '100%'}}
+              placeholder={`궁금한 내용을 질문해주세요 
 -명확한상황 설명과 받고자 하는 답변내용을 적어주세요
 -질문을 보내면 수정/삭제가 불가합니다.
 -모든 이미지 파일은 안전하게 워터마크가 부착되어집니다.
-                `} numberOfLines={4} ellipsizeMode="tail"></TextInput>
-              </View>
-         
-              <File>
-                <FileOpacity>
-                  <Image source={require('../constants/images/question/image.png')} resizeMode="contain"/>
-                  <FileText>사진</FileText>
-                </FileOpacity>
-                <FileOpacity>
-                  <Image source={require('../constants/images/question/video.png')} resizeMode="contain"/>
-                  <FileText>동영상</FileText>
-                </FileOpacity>
-                <FileOpacity>
-                  <Image source={require('../constants/images/question/file.png')} resizeMode="contain"/>
-                  <FileText>파일</FileText>
-                </FileOpacity>
-                <FileOpacity>
-                  <Image source={require('../constants/images/question/voice.png')} resizeMode="contain"/>
-                  <FileText>녹음</FileText>
-                </FileOpacity>
-              </File>
-              
-              <TouchableOpacity style={{width:'80%', height:40,marginTop:30,backgroundColor:'#952bff', borderRadius:4 , alignItems:'center', justifyContent:'center'}}
-                                onPress={()=>{changeModalVisible(true)}}>
-                <Text style={{fontSize:14, fontWeight:'bold', color:'#ffffff'}}>보내기</Text>
-              </TouchableOpacity>
+                `}
+              numberOfLines={4}
+              ellipsizeMode="tail"
+              onChangeText={text => setQuestionText(text)}></TextInput>
+          </View>
 
-           
-              <Modal 
-                transparent={true} animationType='slide' visible={isModalVisible}
-               
-                onDismiss={()=> alert({chooseData}) }
-               
-                >
-                <SimpleModal style={{alignItems:'center', justifyContent:'center'}}
-                  changeModalVisible={changeModalVisible}
-                  setData={setData}/>
-              </Modal>
-           
+          <File>
+            <FileOpacity>
+              <Image
+                source={require('../constants/images/question/image.png')}
+                resizeMode="contain"
+              />
+              <FileText>사진</FileText>
+            </FileOpacity>
+            <FileOpacity>
+              <Image
+                source={require('../constants/images/question/video.png')}
+                resizeMode="contain"
+              />
+              <FileText>동영상</FileText>
+            </FileOpacity>
+            <FileOpacity>
+              <Image
+                source={require('../constants/images/question/file.png')}
+                resizeMode="contain"
+              />
+              <FileText>파일</FileText>
+            </FileOpacity>
+            <FileOpacity>
+              <Image
+                source={require('../constants/images/question/voice.png')}
+                resizeMode="contain"
+              />
+              <FileText>녹음</FileText>
+            </FileOpacity>
+          </File>
 
-            
-            </Contents>
-        </Background>
-      </Container>
-    
-    )
+          <TouchableOpacity
+            style={{
+              width: '80%',
+              height: 40,
+              marginTop: 30,
+              backgroundColor: '#952bff',
+              borderRadius: 4,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            onPress={onSubmit}>
+            <Text style={{fontSize: 14, fontWeight: 'bold', color: '#ffffff'}}>
+              보내기
+            </Text>
+          </TouchableOpacity>
+
+          <Modal
+            transparent={true}
+            animationType="slide"
+            visible={isModalVisible}
+            onDismiss={() => alert({chooseData})}>
+            <SimpleModal
+              style={{alignItems: 'center', justifyContent: 'center'}}
+              changeModalVisible={changeModalVisible}
+              setData={setData}
+            />
+          </Modal>
+        </Contents>
+      </Background>
+    </Container>
+  );
 }
-
 
 export default Question;
