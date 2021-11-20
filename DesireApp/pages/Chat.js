b71fdb354ec2641815712ff53e48c8106b4925ca
Caumannerman <uigohiiming2@gmail.com>
2021-11-21 01:35:50 +0900

    fix: Render sender/recipient profile image

--- DesireApp/pages/Chat.js
+++ DesireApp/pages/Chat.js
@@ -33,15 +33,27 @@ function Chat(props){
 
    const {getAuth} = useAuth();
    const [userId, setUserId] = useState();
+   const [accType, setAccType] = useState('');
   //props로 받은 answerId로 가져온 answer정보를 담을 곳
   const [chatRoomList, setChatRoomList] = useState([]);
 
-  
-  const fetchCRList = async (tempUserId) => {
+//멘티용 
+  const fetchCRList_ME = async (tempUserId) => {
     await ChatRoomService.getList({
       userId,
       ordering: '-created_on',
       senderId: tempUserId,
+    }).then(res => {
+      setChatRoomList(res.data.results);
+      console.log(res.data.results);
+    });
+  };
+//멘토용
+  const fetchCRList_MO = async (tempUserId) => {
+    await ChatRoomService.getList({
+      userId,
+      ordering: '-created_on',
+      recipientId: tempUserId,
     
     }).then(res => {
       setChatRoomList(res.data.results);
@@ -49,12 +61,18 @@ function Chat(props){
     });
   };
 
+
   useEffect(() => {
     (async ()=> {
-      const  {userId} = await getAuth();
+      const  {userId,accType} = await getAuth();
+      console.log(accType,userId)
       setUserId(userId);
+      setAccType(accType);
       const tempUserId = userId;
-      await fetchCRList(tempUserId);
+      
+      {accType==="ME"?
+      await fetchCRList_ME(tempUserId):
+      await fetchCRList_MO(tempUserId)}
     })();
  
   }, []);
@@ -77,14 +95,30 @@ function Chat(props){
                   <TouchableOpacity style={{backgroundColor: '#ffffff', borderWidth: 1, borderColor:'#f5f5f5', width:'100%',  height:HEIGHT*0.1114,flexDirection:'row', alignItems:'center'}}
                     onPress={()=>{
                       const chatroomid = chatroom.id;
-                      props.navigation.navigate("ChatPrivate", {chatroomid:chatroomid, chatrecipient:chatroom.recipient.nickname})}}>
+                      if(accType==="MO"){
+                      props.navigation.navigate("ChatPrivate", {chatroomid:chatroomid, chatnickname:chatroom.sender.nickname, chatid:chatroom.sender.id})}
+                      else{
+                        props.navigation.navigate("ChatPrivate", {chatroomid:chatroomid, chatnickname:chatroom.recipient.nickname, chatid:chatroom.recipient.id})
+                      }
+                        
+                    }}>
                   
-                    <View style={{height: WIDTH*0.1333, width: WIDTH*0.1333, borderRadius: 50,left:WIDTH*0.1138}}>
-                      <Image source={require('../constants/images/homepage/human.png')} resizeMode='contain' style={{width: '100%', height: '100%'}}></Image>
-                    </View>
+                  {accType==="MO"?
+                    <View style={{height: WIDTH*0.1333, width: WIDTH*0.1333, borderRadius: 50,left:WIDTH*0.1138,borderWidth:1, borderColor:'#ffaacc',alignItems:'center',justifyContent:'center'}}>
+                      {chatroom&&chatroom.sender&&chatroom.sender.profile_image&& 
+                      <Image source={{uri:chatroom.sender.profile_image}} resizeMode='cover' style={{width: '100%', height: '100%', borderRadius: 50,backgroundColor:'#ff0000'}}></Image>}
+                    </View>:
+                    
+                    <View style={{height: WIDTH*0.1333, width: WIDTH*0.1333, borderRadius: 50,left:WIDTH*0.1138,borderWidth:1, borderColor:'#ffaacc',alignItems:'center',justifyContent:'center'}}>
+                    {chatroom&&chatroom.recipient&&chatroom.recipient.profile_image&& 
+                    <Image source={{uri:chatroom.recipient.profile_image}} resizeMode='cover' style={{width: '100%', height: '100%', borderRadius: 50}}></Image>}
+                   </View>
+                    }
                     
                     <View style={{flexDirection: 'column' , left:WIDTH*0.1606, width:WIDTH*0.5183, height:HEIGHT*0.0566, justifyContent:'space-between'}}>
-                      <Text style={{fontSize: WIDTH*0.035,fontWeight:'bold', width:WIDTH*0.4833, color:'#000000'}}>{chatroom&&chatroom.sender&&chatroom.recipient.nickname}</Text>
+                    {accType==="ME"?
+                      <Text style={{fontSize: WIDTH*0.035,fontWeight:'bold', width:WIDTH*0.4833, color:'#000000'}}>{chatroom&&chatroom.recipient&&chatroom.recipient.nickname}</Text>:
+                      <Text style={{fontSize: WIDTH*0.035,fontWeight:'bold', width:WIDTH*0.4833, color:'#000000'}}>{chatroom&&chatroom.sender&&chatroom.sender.nickname}</Text>}
                       <Text style={{fontSize: 13,fontWeight:'300', width:WIDTH*0.4833, height: HEIGHT*0.0243,color:'#858585'}}>{chatroom&&chatroom.latest_chat_message&&chatroom.latest_chat_message.content}</Text>
                     </View>
 
